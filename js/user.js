fd817f4f3d9e81451c5ff3a411dc2659a82e8397
alhn <alhn.gzl@gmail.com>
2015-07-22 01:36:25 +0300

    My Page

---
+++ js/user.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	$("#play").click(function(){
+		$("#play_a").animate({width: "800px"},500)
+	});
+	$("#design").click(function(){
+		$("#design_a").animate({width: "800px"},500)
+	});
+	$("#gist").click(function(){
+		$("#gist_a").animate({width: "800px"},500)
+	});
+	$("#gmail").click(function(){
+		$("#gmail_a").animate({width: "800px"},500)
+	});
+	$("#linkedin").click(function(){
+		$("#linkedin_a").animate({width: "800px"},500)
+	});
+	$("#sites").click(function(){
+		$("#sites_a").animate({width: "800px"},500)
+	});
+})
